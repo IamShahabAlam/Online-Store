2ca1e1772bc96bf75707a300a96ad5e77b19b11c
Shahab Alam <iamshahabalam@gmail.com>
2022-07-19 13:14:52 +0500

    Initial Commit

--- lib/screens/post_product.dart
+++ lib/screens/post_product.dart
@@ -1,6 +1,8 @@
 import 'package:appUp_one/components/constants.dart';
 import 'package:clippy_flutter/buttcheek.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'dart:ui';
 
 class Post_Product extends StatelessWidget {
 
@@ -17,38 +19,48 @@ class Post_Product extends StatelessWidget {
           Stack(
             children: [
               ButtCheek(
-                height: 30,
-                child: Image.asset("assets/cover.jpg",
-                  height: 200,
-                  width: double.infinity,
-                  fit: BoxFit.cover,),
+                height: 25,
+                child: ImageFiltered(
+                  imageFilter: ImageFilter.blur(sigmaY: 5.0 ,sigmaX: 5.0),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.black.withOpacity(0.1),
+                    ),
+                    child: Image.asset("assets/cover.jpg",
+                      height: 150,
+                      width: double.infinity,
+                      fit: BoxFit.cover,),
+                  ),
+                ),
               ),
 
-              Positioned( top: 60, left: _w*0.38 ,
-                  child: Container( height: 90.0, width: 90.0,
-                      child: Image.asset("assets/logo.png" , fit: BoxFit.contain,))),
+              Positioned( top: 50, left: _w*0.29 ,
+                  child: Container( height: 90.0, width: 200.0,
+                      child: Text("POST AD" , style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold , fontSize: 40.0, letterSpacing: 2.0),))),
 
               Positioned( top: 30.0 , left: 10.0,
                 child: Container(height: 40, width: 40,
-                  padding: EdgeInsets.only(right: 15.0),
+                  alignment: Alignment.center,
                   decoration: BoxDecoration(
                     color: Colors.white,
                     shape: BoxShape.circle,
 
+
                   ),
-                  child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(
+                  child: TextButton(
+                      style: TextButton.styleFrom(
                           primary: Colors.transparent,
-                          // shape: CircleBorder()
+                          elevation: 0,
                       ),
-                      onPressed: (){},
-                      child: Icon(Icons.arrow_back_ios_outlined, size: 25.0, color: kBMRactiveCardColor,)),
+                      onPressed: (){Navigator.pop(context);},
+                      child: Icon(Icons.arrow_back_ios_outlined, color: kBMRactiveCardColor,)),
                 ),
               ),
             ],
           ),
-
 // =============================================================================================
+
+
 
         ],
       ),
